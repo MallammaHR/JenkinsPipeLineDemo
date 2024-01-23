080d02741e79f8935249b2d95b1265a39f81aa24
MallammaHR <mallumanish17@gmail.com>
2024-01-23 10:33:46 -0600

    Korgajja............

---
+++ src/test/java/com/basics/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld 
+{
+	@Test
+	public void printMessage()
+	{
+		System.out.println("HelloWorld!!!!!!!!!!!!!!!!!!!");
+	}
+}
